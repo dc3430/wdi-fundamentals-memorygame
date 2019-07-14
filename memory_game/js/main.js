ec8a45f2bce1bd82265e5f0158832665ff4cb8b8
DC <decermak@us.ibm.com>
2019-07-14 09:02:28 -0500

    add card0

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,17 @@
 console.log("Up and running!");
 
+/*
 var cardOne =  "queen";
 var cardTwo =  "king";
 var cardThree =  "queen";
 var cardFour =  "king";
 
 console.log("User flipped + cardOne");
+*/
+
+var cards = ["queen", "quene", "king", "king"];
+
+var cardOne = [0];
+cardsInPlay.push('cardOne');
+
+console.log();
