0d81030c9cc18138c07be05ccb18ee33c6153b62
DC <decermak@us.ibm.com>
2019-07-14 15:56:56 -0500

    add.func.flip

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,3 +1,18 @@
+var cards = ["queen", "quene", "king", "king"];
+
+var cardsInPlay = [];
+
+//user flips a card
+funtction flipcard () {
+	var cards = cardID;
+	console.log("User flipped " + (cards[cardId].rack)); 
+	cardsInPlay.push(cards[cardId].rack);
+	
+}
+
+flipcard(0);
+flipcard(2);
+
 
 
 /*
@@ -9,19 +24,16 @@ var cardFour =  "king";
 console.log("User flipped + cardOne");
 */
 
-var cards = ["queen", "quene", "king", "king"];
-
-var cardsInPlay = [];
-
-var cardOne = [0];
+/*var cardOne = [0];
 cardsInPlay.push('cardOne');
 console.log("User Flipped " + cardOne);
 
 var cardTwo = [2];
 cardsInPlay.push('cardTwo');
-console.log("User Flipped " + cardTwo);
+console.log("User Flipped " + cardTwo); */
 
 //Check for match
+/*
 if (cardsInPlay.length === 2) {
   if( cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
@@ -30,6 +42,6 @@ if (cardsInPlay.length === 2) {
       alert("Sorry, try again.");
       console.log(cardsInPlay);
     }
-}
+}  */
 
 
