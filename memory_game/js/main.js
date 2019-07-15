ead3c4276a17b181bba42fa87123f2a3f76d451f
DC <decermak@us.ibm.com>
2019-07-14 20:41:45 -0500

    created.game.logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,55 +1,43 @@
-var cards = ["queen", "quene", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "heart",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-dimonds.png"
+},
+{
+	rank: "king",
+	suit: "heart",
+	cardImage: "images/king-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "diamonds"
+	cardImage: "images/king-of-diamonds.png"
+},
+];
 
 var cardsInPlay = [];
 
-function checkForMatch () {    //checking for match
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
-} else {
-  console.log("Sorry, try again.");
-}
+var flipCard = function() {
+    var cardId = parseInt(this.getAttribute('data-id'),10); // use parseInt to convert to integer
+    console.log("User flipped " + (cards[cardId].rank));
+    this.src = cards[cardId].cardImage; // add this to set the image
+    cardsInPlay.push(cards[cardId].rank);
+    checkForMatch();
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+};
 
-
-funtction flipcard () {    //user flips a card
-	var cards = cardID;
-	console.log("User flipped " + (cards[cardId].rack)); 
-	cardsInPlay.push(cards[cardId].rack);
-	checkForMatch();
-	
-}
-
-flipcard(0);
-flipcard(2);
-
-
-
-/*
-var cardOne =  "queen";
-var cardTwo =  "king";
-var cardThree =  "queen";
-var cardFour =  "king";
-
-console.log("User flipped + cardOne");
-*/
-
-/*var cardOne = [0];
-cardsInPlay.push('cardOne');
-console.log("User Flipped " + cardOne);
-
-var cardTwo = [2];
-cardsInPlay.push('cardTwo');
-console.log("User Flipped " + cardTwo); */
-
-//Check for match
-/*
-if (cardsInPlay.length === 2) {
-  if( cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  }
-    else{
-      alert("Sorry, try again.");
-      console.log(cardsInPlay);
+var checkForMatch = function() {
+    this['src'] = (cards[0,1,2,3].cardImage);
+    if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again");
     }
-}  */
-
-
+};
