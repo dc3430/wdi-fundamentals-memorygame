eeb0693572a6542d8878c749f79a0e684863cff8
DC <decermak@us.ibm.com>
2019-07-16 08:50:45 -0500

    working game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,55 +1,74 @@
 var cards = [
-{
-	rank: "queen",
-	suit: "heart",
-	cardImage: "images/queen-of-hearts.png"
-},
-{
-	rank: "queen",
-	suit: "diamonds",
-	cardImage: "images/queen-of-dimonds.png"
-},
-{
-	rank: "king",
-	suit: "heart",
-	cardImage: "images/king-of-diamonds.png"
-},
-{
-	rank: "king",
-	suit: "diamonds"
-	cardImage: "images/king-of-diamonds.png"
-},
+    {
+        rank: "queen",
+        suit: "heart",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "heart",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    },
 ];
 
 var cardsInPlay = [];
 
 var flipCard = function() {
-    var cardElement = this.getAttribute('data-id');
+    var cardId = this.getAttribute('data-id'); //cardId assumed in the array order O-3 like an HDD, "this = cards" from line 1, The getAttribute() method returns the value of the attribute with the specified name, of an element.
     console.log("User flipped " + (cards[cardId].rank));
-    cardsInPlay.push(cards[cardID].rank);
+    cardsInPlay.push(cards[cardId].rank);
+
+    this.setAttribute('src', cards[cardId].cardImage) //the only way i can get the cards to show up the html page
+
     checkForMatch();
     console.log(cards[cardId].cardImage);
     console.log(cards[cardId].suit);
 };
 
 var checkForMatch = function() {
-    this['src'] = (cards[0,1,2,3].cardImage);
-    cardElement.setAttribute(src[cardId].cardImage);
-    if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found a match!");
-    } else {
-        alert("Sorry, try again");
+    if (cardsInPlay.length === 2) { //two card need to be in play
+        if (cardsInPlay[0] === cardsInPlay[1]) { //Id0card = Id1card
+            alert("You found a match!");
+        } else {
+            alert("Sorry, try again");
+        }
+        // reset()
     }
 };
 
+
 var createBoard = function() {
-    for (var i = 0; i < cards.length;i++){
-        var cardElement = document.create.Element('img');
-        cardElement.setAttribute('src', "image/back.png");
+    // place all facedown
+    for (var i = 0; i < cards.length; i++) {
+
+        var cardElement = document.createElement('img');
+        // console.log(cardElement);
+        cardElement.setAttribute('src', "images/back.png");
         cardElement.setAttribute('data-id', i);
+
         cardElement.addEventListener('click', flipCard);
         document.getElementById('game-board').appendChild(cardElement);
+        console.log(cardElement);
     }
 }
 
+// var reset = function() {
+//     cardsInPlay = [];
+//     createBoard()
+//     document.re
+//     return "hello return"
+// }
+
 createBoard();
+// console.log('hello')
+
